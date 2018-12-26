2d22a7a378926225f5959153deda7e43adc46aa8
yuina_1056 <yuina.i.1056@gmail.com>
2018-12-26 18:15:14 +0900

    first commit

---
+++ spec/calc_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../src/calc.rb'
+
+RSpec.describe '計算' do
+  before do
+    @calc = Calc.new
+  end
+  it '1 + 1 = 2 ?' do
+    calc = Calc.new
+    expect(@calc.add(1,1)).to equal(2)
+  end
+  it '2 + 5 = 7 ?' do
+    expect(@calc.add(2,5)).to equal(7)
+  end
+end
---
+++ src/calc.rb
@@ -0,0 +1,5 @@
+class Calc
+  def add(a,b)
+    a + b
+  end
+end
